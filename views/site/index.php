e12dcbd66586533d936cb6c1367d8da60af6b35b
denis <mdn81@ukr.net>
2017-02-21 19:03:11 +0200

    master commit test

--- views/site/index.php
+++ views/site/index.php
@@ -147,7 +147,7 @@ use app\models\Article;
                             $gallery = $article->getImages();
                             $catImg = $article->category->getImage();
                             ?>
-                        мастер
+                        мастер тест 
                             <div class="col-sm-6">
                                 <div class="articles">
                                     <div class="articles-gen-img">
